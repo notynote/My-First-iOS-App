25e650a49414d1e4b8125e71c9804399db83155d
notynote <32334875+notynote@users.noreply.github.com>
2020-10-21 10:12:57 +0700

    Initial Commit

---
+++ MyFirstApp/MyFirstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstAppApp.swift
+//  MyFirstApp
+//
+//  Created by Pathompong Phongsaporamut on 21/10/2563 BE.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
