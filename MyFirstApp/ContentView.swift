8cff90fedf697702feeeb745c166a331def16e78
notynote <32334875+notynote@users.noreply.github.com>
2020-10-21 11:16:13 +0700

    Create menu randomizer

--- MyFirstApp/ContentView.swift
+++ MyFirstApp/ContentView.swift
@@ -8,9 +8,33 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var name:String = "วันนี้กินอะไรดี"
+    @State var rand:Int = 0
+    @State var count:Int = 0
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text(name)
+                .font(.largeTitle)
+                .foregroundColor(.red)
+                .frame(width: 300, height: 300, alignment: .center)
+                .padding()
+            Button(action: {
+                
+                self.rand = Int.random(in: 0...4)
+                
+                let menuList = ["กะเพราหมูกรอบ","ข้าวผัดปู","ข้าวมันไก่","ส้มตำ","ข้าวหมูแดง"]
+                
+                self.name = menuList[rand]
+                
+                self.count += 1
+                
+            }) {
+                Text("สุ่มเมนู")
+            }.frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: 70, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+            Text("สุ่มไปแล้ว " + String(count) + " ครั้ง")
+        }
     }
 }
 
